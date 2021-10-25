2df65d34dfaa009ebcd1464c970ca0579df4509b
sava <savarozsnyai@gmail.com>
2021-10-25 09:52:31 -0700

    Create debug.cpp
    all hail debug code

---
+++ src/debug.cpp
@@ -0,0 +1,18 @@
+#include <CrcLib.h>
+
+using namespace Crc;
+
+void setup() {
+    CrcLib::Initialize();
+
+    Serial.begin(9600);
+    
+}
+void loop() {
+    CrcLib::Update();
+)
+    Serial.print("The code execution time is ");
+    Serial.print(CrcLib::GetDeltaTimeMillis());
+    Serial.println(" ms.");
+    
+}

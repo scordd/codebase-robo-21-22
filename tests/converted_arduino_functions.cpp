4c078e7d4abf5ad9c8e20994dc050c08220dca86
Tojo-hung <xracerh@gmail.com>
2021-11-01 18:07:48 +0000

    added mini functions

---
+++ tests/converted_arduino_functions.cpp
@@ -0,0 +1,43 @@
+#include <CrcLib.h>
+
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+
+int HC_SR04(int trigPin, int echoPin){
+    CrcLib::Update()
+
+    long sent;
+    long got;
+
+    long duration; 
+    int distance; 
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = got - sent;
+
+    distance = duration*0.034/2;
+
+    return distance;
+}
+
+int wait(int time){
+    CrcLib::Update();
+    
+    int x = time/5;
+
+    for(int i; i <= x; i++){
+        CrcLib::Update();
+        delay(5);
+    }
+
+}

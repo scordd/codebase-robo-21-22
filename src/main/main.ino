353622fe3a55954b523c2c7d455dba1b6068dacc
Sava Rozsnyai <savarozsnyai@gmail.com>
2022-02-02 16:05:19 -0800

    updates

--- src/main/main.ino
+++ src/main/main.ino
@@ -17,16 +17,16 @@ int mode = 1; // Focus on getting the robot to move for now, then we'll add mode
 void setup()
 {
   CrcLib::Initialize();
-  Serial.begin(19200);
+  Serial.begin(9200);
 
   CrcLib::InitializePwmOutput(LF);
   CrcLib::InitializePwmOutput(RF);
   CrcLib::InitializePwmOutput(BL);
   CrcLib::InitializePwmOutput(BR);
-  
+
   CrcLib::InitializePwmOutput(ARM1);
   CrcLib::InitializePwmOutput(ARM2);
-  
+
   CrcLib::InitializePwmOutput(Servo12inch);
   CrcLib::InitializePwmOutput(Servo18inch);
   CrcLib::InitializePwmOutput(Servo24inch);
@@ -52,128 +52,143 @@ void loop()
     int r1analog = CrcLib::ReadDigitalChannel(BUTTON::R1);
     int r2trigger = CrcLib::ReadAnalogChannel(ANALOG::GACHETTE_R);
 
-    int right = CrcLib::ReadDigitalChannel(BUTTON::COLORS_RIGHT);
-    int left = CrcLib::ReadDigitalChannel(BUTTON::COLORS_LEFT);
-    
+    bool right = CrcLib::ReadDigitalChannel(BUTTON::ARROW_RIGHT);
+    bool left = CrcLib::ReadDigitalChannel(BUTTON::ARROW_LEFT);
+    bool down = CrcLib::ReadDigitalChannel(BUTTON::ARROW_DOWN);
+
     CrcLib::Update();
-    
-    if (j2xpos == 0 && j1ypos == 0 && l2trigger == 0 && r2trigger == 0){
-      done();  
+
+    // END ALL TRANSMISSION FROM JOYSTICKS AND L2 R2 TRIGGERS
+    if (j2xpos == 0 && j1ypos == 0 || l2trigger == -128 && r2trigger == -128 || left == LOW && right == LOW && down == LOW) {
+      done();
     }
-    
-    if (l2trigger != 0 || r2trigger != 0){
-      if (l2trigger < 50) {
+
+    // Tank drive forward-backward (LF, BL, RF, BR)
+    if (l2trigger != -128 || r2trigger != -128) {
+      if (l2trigger < -100) {
         turnl(100);
       }
-      else if (r2trigger < 50) {
+      else if (r2trigger < -100) {
         turnr(100);
       }
     }
-    
-    if (j1ypos != 0 || j1xpos != 0){
-      if (j1ypos < -126){
-         forward(100);
+
+    // Lateral movement/rotation
+    if (j1ypos != 0 || j1xpos != 0) {
+
+      if (j1ypos < -126) {
+        forward(100);
       }
-      else if(j1ypos > 126){
-         backward(100);
+      else if (j1ypos > 126) {
+        backward(100);
       }
-//       if (j1xpos < -126){
-//          latl(100);
-//       }
-//       else if(j1xpos > 126){
-//          latr(100);
-//       }
+      if (j1xpos < -126) {
+        latl(100);
+      }
+      else if (j1xpos > 126) {
+        latr(100);
+      }
     }
 
+    // Servo control
+    if (left == HIGH || right == HIGH || down == HIGH) {
+      if (left == HIGH) {
+        CrcLib::InitializePwmOutput(Servo12inch, 10);
+      }
+      else if (right == HIGH) {
+        CrcLib::InitializePwmOutput(Servo16inch, 10);
+      }
+      else if (down == HIGH) {
+        CrcLib::InitializePwmOutput(Servo24inch, 10);
+      }
+    }
 
-
-    
+    else
+    {
+      Serial.print("No controller connected, file will not work.");
+      CrcLib::Update();
+    }
   }
-  else
-  {
-    Serial.print("No controller connected, file will not work.");
-    CrcLib::Update();
-  }
-}
-  
-  
-void forward(int speed){
-       CrcLib::SetPwmOutput(LF, speed);
-       CrcLib::SetPwmOutput(BL, speed);
-       CrcLib::SetPwmOutput(RF, -speed);
-       CrcLib::SetPwmOutput(BR, -speed);
-       CrcLib::Update();
 }
 
-void backward(int speed){
-       CrcLib::SetPwmOutput(LF, -speed);
-       CrcLib::SetPwmOutput(BL, -speed);
-       CrcLib::SetPwmOutput(RF, speed);
-       CrcLib::SetPwmOutput(BR, speed);
-       CrcLib::Update();
-}  
 
-void turnr(int speed){
-       CrcLib::SetPwmOutput(LF, speed);
-       CrcLib::SetPwmOutput(BL, speed);
-       CrcLib::SetPwmOutput(RF, speed);
-       CrcLib::SetPwmOutput(BR, speed);
-       CrcLib::Update();
-}
-  
-void turnl(int speed){
-       CrcLib::SetPwmOutput(LF, -speed);
-       CrcLib::SetPwmOutput(BL, -speed);
-       CrcLib::SetPwmOutput(RF, -speed);
-       CrcLib::SetPwmOutput(BR, -speed);
-       CrcLib::Update();
-}
-
-void latr(int speed){
-       CrcLib::SetPwmOutput(LF, speed);
-       CrcLib::SetPwmOutput(BL, -speed);
-       CrcLib::SetPwmOutput(RF, -speed);
-       CrcLib::SetPwmOutput(BR, speed);
-       CrcLib::Update();
-}
-
-void latl(int speed){
-       CrcLib::SetPwmOutput(LF, -speed);
-       CrcLib::SetPwmOutput(BL, speed);
-       CrcLib::SetPwmOutput(RF, speed);
-       CrcLib::SetPwmOutput(BR, -speed);
-       CrcLib::Update();
-}
-
-void done(){
-       CrcLib::SetPwmOutput(LF, 0);
-       CrcLib::SetPwmOutput(BL, 0);
-       CrcLib::SetPwmOutput(RF, 0);
-       CrcLib::SetPwmOutput(BR, 0);
-       CrcLib::Update();
-}
-
-void wait(int a){
+void forward(int speed) {
+  CrcLib::SetPwmOutput(LF, speed);
+  CrcLib::SetPwmOutput(BL, speed);
+  CrcLib::SetPwmOutput(RF, -speed);
+  CrcLib::SetPwmOutput(BR, -speed);
   CrcLib::Update();
-  unsigned long x = millis(); 
+}
+
+void backward(int speed) {
+  CrcLib::SetPwmOutput(LF, -speed);
+  CrcLib::SetPwmOutput(BL, -speed);
+  CrcLib::SetPwmOutput(RF, speed);
+  CrcLib::SetPwmOutput(BR, speed);
+  CrcLib::Update();
+}
+
+void turnr(int speed) {
+  CrcLib::SetPwmOutput(LF, speed);
+  CrcLib::SetPwmOutput(BL, speed);
+  CrcLib::SetPwmOutput(RF, speed);
+  CrcLib::SetPwmOutput(BR, speed);
+  CrcLib::Update();
+}
+
+void turnl(int speed) {
+  CrcLib::SetPwmOutput(LF, -speed);
+  CrcLib::SetPwmOutput(BL, -speed);
+  CrcLib::SetPwmOutput(RF, -speed);
+  CrcLib::SetPwmOutput(BR, -speed);
+  CrcLib::Update();
+}
+
+void latr(int speed) {
+  CrcLib::SetPwmOutput(LF, speed);
+  CrcLib::SetPwmOutput(BL, -speed);
+  CrcLib::SetPwmOutput(RF, -speed);
+  CrcLib::SetPwmOutput(BR, speed);
+  CrcLib::Update();
+}
+
+void latl(int speed) {
+  CrcLib::SetPwmOutput(LF, -speed);
+  CrcLib::SetPwmOutput(BL, speed);
+  CrcLib::SetPwmOutput(RF, speed);
+  CrcLib::SetPwmOutput(BR, -speed);
+  CrcLib::Update();
+}
+
+void done() {
+  CrcLib::SetPwmOutput(LF, 0);
+  CrcLib::SetPwmOutput(BL, 0);
+  CrcLib::SetPwmOutput(RF, 0);
+  CrcLib::SetPwmOutput(BR, 0);
+  CrcLib::Update();
+}
+
+void wait(int a) {
+  CrcLib::Update();
+  unsigned long x = millis();
   unsigned long y = x + a;
-        
-  while(x < y){
+
+  while (x < y) {
     CrcLib::Update();
     x = millis();
   };
-            
+
 };
 
-void waitfast(int a){
+void waitfast(int a) {
   CrcLib::Update();
-  unsigned long x = micros(); 
+  unsigned long x = micros();
   unsigned long y = x + a;
-        
-  while(x < y){
+
+  while (x < y) {
     CrcLib::Update();
     x = micros();
   };
-            
+
 };
 // Functions here? Or maybe a library....
